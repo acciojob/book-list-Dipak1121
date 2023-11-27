944e19dce7dd57aea0671e14323586f2e4b36ff3
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-27 12:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", onClick);
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let isbn = document.getElementById("isbn");
+let tbody = document.getElementById("book-list");
+function onClick(){
+	let tr = document.createElement("tr");
+	tr.innerHTML = `<td>${title.value}</td><td>${author.value}</td><td>${isbn.value}</td><td><button class="cancel-btn" onClick="onCancelBtn(this)">X</button></td>`;
+	tbody.appendChild(tr);
+}
+
+function onCancelBtn(element){
+    element.parentNode.parentNode.remove();
+}
